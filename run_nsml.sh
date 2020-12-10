47da60613258b4dad89aa494e959e1d4026edca4
HyeongshinYoon <hyeongshin@LAPTOP-9V0BF5NJ.localdomain>
2020-12-10 14:54:54 +0900

    add example style and parser

--- run_nsml.sh
+++ run_nsml.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 nsml run \
-  -m 'koelectra (pretrained : base(v3), per_qa_paragraph_cnt > 3, epoch: 20, fp16)' \
+  -m 'koelectra (pretrained : base(v3), per_qa_paragraph_cnt > 3, epoch: 2, dropuout (0.3, 0.3, 0.0))' \
   -d korquad-open-ldbd3 \
   -g 1 \
   -c 2 \
@@ -11,10 +11,11 @@ nsml run \
     --do_train
     --do_eval
     --data_dir train
-    --num_train_epochs 20
+    --num_train_epochs 2
     --per_gpu_train_batch_size 24
     --per_gpu_eval_batch_size 24
     --verbose_logging
     --output_dir output
     --overwrite_output_dir
-    --version_2_with_negative"
+    --version_2_with_negative
+    --example_style iter"
